ed2067192a8130ae1e1d4019f8deb73afdadfdd7
AlejandroIquira27 <alejoiquira.27@gmail.com>
2023-05-22 16:02:21 -0500

    Se agrega el Proyecto

---
+++ src/main/java/com/mycompany/ejercicio18/Ret.java
@@ -0,0 +1,14 @@
+
+package com.mycompany.ejercicio18;
+
+
+public class Ret {
+    
+    public static double Retencion(double dato1){
+    
+        double rete = dato1 / 100;
+        return rete;
+        
+    }
+    
+}
