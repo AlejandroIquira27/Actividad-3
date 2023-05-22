e3f771abb82870855ef7bb76b49dc1754cda4338
AlejandroIquira27 <126117636+AlejandroIquira27@users.noreply.github.com>
2023-05-22 16:25:47 -0500

    Add files via upload

---
+++ ejercicio22/Ejercicio22.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.ejercicio22;
+
+public class Ejercicio22 {
+
+    public static void main(String[] args) {
+        
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
+        
+    }
+}
