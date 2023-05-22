c7454cbd4c61f474cfa1386b6ff81c4007973107
AlejandroIquira27 <126117636+AlejandroIquira27@users.noreply.github.com>
2023-05-22 16:38:01 -0500

    Add files via upload

---
+++ ejercicio22/Salario.java
@@ -0,0 +1,13 @@
+
+package com.mycompany.ejercicio22;
+
+public class Salario {
+    
+    public static double Salario_Mensual(double ValorHr, double HrsTrab){
+    
+        double total = ValorHr * HrsTrab;
+        return total;
+        
+    }
+    
+}
