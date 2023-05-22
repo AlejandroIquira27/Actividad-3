13525f2b3cd65447f07be3d6323e0c075e5a403e
AlejandroIquira27 <126117636+AlejandroIquira27@users.noreply.github.com>
2023-05-22 16:30:10 -0500

    Add files via upload

---
+++ ejercicio18/Ejercicio18.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.ejercicio18;
+
+
+public class Ejercicio18 {
+
+    public static void main(String[] args) {
+        
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
+        
+    }
+}
---
+++ ejercicio18/SalBruto.java
@@ -0,0 +1,13 @@
+package com.mycompany.ejercicio18;
+
+
+public class SalBruto {
+    
+    public static double Salario_Bruto(double dato1, double dato2){
+    
+        double sal = dato1 * dato2;
+        return sal;
+        
+    }
+    
+}
